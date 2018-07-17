9805372ed33fb2fc73df28eded71c5ab8fd8f1a2
Christopher Stone <chstone@microsoft.com>
2018-07-17 15:00:47 -0400

    simplify example code (#1)
    * simplify example code
    
    * cleanup
    
    * spelling

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { BotFrameworkAdapter, MemoryStorage } = require('botbuilder');
-const { DialogSet } = require('botbuilder-dialogs');
-const BotStateManager = require('./botStateManager');
-
-var express = require('express')
-const moment = require('moment');
-const _ = require('lodash');
-
+const express = require('express')
+const { BotFrameworkAdapter, ActivityTypes } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
 
-const appId = '<YOUR-APP-ID>';
-const subscriptionKey = '<YOUR-SUBSCRIPTION-KEY>';
+const { GREETING, EXAMPLES } = require('./text');
 
-// Default is westus
-const serviceEndpoint = 'https://westus.api.cognitive.microsoft.com';
+const luisAppId = '<YOUR-APP-ID>';
+const luisSubscriptionKey = '<YOUR-SUBSCRIPTION-KEY>';
+const port = process.env.port || process.env.PORT || 3978;
+const luisServiceEndpoint = 'https://westus.api.cognitive.microsoft.com';
 
 const model = new LuisRecognizer({
-    appId: appId,
-    subscriptionKey: subscriptionKey,
-    serviceEndpoint: serviceEndpoint
+    appId: luisAppId,
+    subscriptionKey: luisSubscriptionKey,
+    serviceEndpoint: luisServiceEndpoint
 });
 
 // Create server
-let server = express();
-let port = process.env.port || process.env.PORT || 3978;
-server.listen(port, function() {
-    console.log(`${server.name} listening on ${port}`);
-});
+const server = express();
+server.listen(port, () => console.log(`${server.name} listening on ${port}`));
 
 // Create adapter
 const adapter = new BotFrameworkAdapter({
@@ -36,81 +28,34 @@ const adapter = new BotFrameworkAdapter({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Add state middleware
-const state = new BotStateManager(new MemoryStorage());
-adapter.use(state);
+// Handler for every conversation turn
+const botLogic = async (context) => {
+    if (context.activity.type === ActivityTypes.ConversationUpdate && context.activity.membersAdded[0].name === 'Bot') {
+        // Welcome message here, bot will message you first
+        await context.sendActivity(GREETING);
+        await context.sendActivity(EXAMPLES);
+    } else if (context.activity.type === 'message') {
+        const utterance = (context.activity.text || '').trim().toLowerCase();
+        console.log(`utterance: ${utterance}`);
 
-function isEmpty(str) {
-    return (!str || 0 === str.length);
+        const luisResult = await model.recognize(context);
+        const topIntent = LuisRecognizer.topIntent(luisResult);
+        const topIntentScore = luisResult.intents[topIntent].score;
+        const entities = Object.values(luisResult.entities)
+            .map((x) => x[0])
+            .map((x) => x.type === 'daterange' || x.type === 'date' ? x.timex[0] : x);
+        console.log(`entities: ${entities}`);
+
+        const response = [
+            `top intent: **${topIntent || 'n/a'}**`,
+            `confidence score: **${topIntentScore || 'n/a'}**`,
+            `entities: **${entities.join(', ') || 'n/a'}**`,
+        ].join('\n');
+        await context.sendActivity(response);
+    }
 }
 
 // Listen for incoming requests
-server.post('/api/messages', (req, res) => {
-    // Route received request to adapter for processing
-    adapter.processActivity(req, res, async context => {
-        if (context.activity.type === 'conversationUpdate' && context.activity.membersAdded[0].name === 'Bot') {
-            // Welcome message here, bot will message you first
-            await context.sendActivity(`Hello! I'm a simple project planning bot for the AzureCAT WWAH. I'll show you the top scoring intent, confidence score, and entities for what you type.`);
-            let examples = `Some examples:`;
-            examples += `\n- Show me all docs for Wipro`;
-            examples += `\n- Show me all people with Java programming skills`;
-            examples += `\n- Show me all people who are free from September 2018 to November 2018`;
-            examples += `\n- How are you?`;
-            examples += `\n- Help`;
-            examples += `\n- What is the weather like today?`;
-            await context.sendActivity(examples);
-        } else if (context.activity.type === 'message') {
-            const utterance = (context.activity.text || '').trim().toLowerCase();
-            console.log('utterance:' + utterance);
-
-            // Create dialog context
-            const dc = dialogs.createContext(context, state);
-
-            let topIntent;
-            let topIntentScore;
-            let entities = [];
-
-            // Call LUIS model
-            await model
-                .recognize(context)
-                .then(res => {
-                    // Resolve intents and entities returned from LUIS
-                    topIntent = LuisRecognizer.topIntent(res);
-                    console.log('topIntent: ' + topIntent);
-                    topIntentScore = res.intents[topIntent].score
-                    console.log('topIntentScore: ' + topIntentScore);
-                    let rawEntities = Object.values(res.entities);
-                    let entity;
-                    for (let i = 0; i < rawEntities.length; i++) {
-                        entity = rawEntities[i];
-                        let type = entity[0].type;
-                        if ( (type === 'daterange') || (type === 'date') ) {
-                            entity = entity[0].timex[0];
-                        }
-                        entities.push(entity);
-                    }
-                    console.log('entities: ' + entities);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-
-            let response = 'top intent:';
-            if (!isEmpty(topIntent)) {
-                response += '**' + topIntent + '**';
-            }
-            response += '\nconfidence score: ';
-            if (!isEmpty(topIntentScore)) {
-                response += '**' + topIntentScore + '**';
-            }
-            response += '\nentities: ';
-            if (!isEmpty(entities)) {
-                response += '**' + entities + '**';
-            }
-
-            await context.sendActivity(response);
-        }
-    });
+server.post('/api/messages', (req, res, next) => {
+    adapter.processActivity(req, res, botLogic).catch(next);
 });
-
-const dialogs = new DialogSet();
