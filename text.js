235b989d273c2b70de26943d2c55b0b97442c637
Chris Stone <chstone@microsoft.com>
2018-07-17 14:21:31 -0400

    simplify example code

---
+++ text.js
@@ -0,0 +1,9 @@
+exports.GREETING = `Hello! I'm a simple project planning bot for the AzureCAT WWAH. I'll show you the top scoring intent, confidence score, and entities for what you type.`;
+
+exports.EXAMPLES = `Some examples:
+- Show me all docs for Wipro
+- Show me all people with Java programming skills
+- Show me all people who are free from September 2018 to November 2018
+- How are you?
+- Help
+- What is the weather like today?`;
